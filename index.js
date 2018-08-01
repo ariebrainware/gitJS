c9cb0c8439eac31e3fa416a881cb2ee817d6efee
Arie Brainware <ariebrainware@gmail.com>
2018-08-01 14:52:16 +0700

    Fix conditional statement if for coloring output

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // Import module
 const calc = require('./lib/math')
-const color = require('color')
+const colors = require('colors')
 const readline = require('readline')
 
 // Function definition
-const colorState = (value = off) => {
-    if (value === off) {
-        console.log(color.green(`4 + 2 = ${calc.add(4,2)}`))
-        console.log(color.rainbow(`8 - 12 = ${calc.substract(8,12)}`))
-        console.log(color.rainbow(`9 / 3 = ${calc.divide(9,3)}`))
-        console.log(color.rainbow(`10 * 89 = ${calc.multiply(10,89)}`))
-        console.log(color.rainbow(`40 % 3 = ${calc.modulo(40,3)}`))
+const colorState = (value = "off") => {
+    if (value == "on") {
+        console.log(colors.green(`4 + 2 = ${calc.add(4,2)}`))
+        console.log(colors.rainbow(`8 - 12 = ${calc.substract(8,12)}`))
+        console.log(colors.rainbow(`9 / 3 = ${calc.divide(9,3)}`))
+        console.log(colors.rainbow(`10 * 89 = ${calc.multiply(10,89)}`))
+        console.log(colors.rainbow(`40 % 3 = ${calc.modulo(40,3)}`))
+        
     } else {
         console.log(`4 + 2 = ${calc.add(4,2)}`)
         console.log(`8 - 12 = ${calc.substract(8,12)}`)
@@ -27,6 +28,7 @@ const input = readline.createInterface({
 })
 
 input.question('Do you want colorful output?: ', (answer) =>{
-    if(answer == "yes") colorState(on)
-    else colorState(off)
+    if(answer == 'yes'){ colorState("on")} 
+    else if(answer == 'no'){ colorState("off")}
+    input.close()
 } )
