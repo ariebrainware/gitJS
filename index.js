7a3337f8a4053d159220468407b15b6e94de5b0b
Arie Brainware <ariebrainware@gmail.com>
2018-08-01 16:02:52 +0700

    Add some explanation about the output and add node-fetch lib

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const calc = require('./lib/math')
 const colors = require('colors')
 const readline = require('readline')
+const fetch = require('node-fetch')
+
+
+console.log(colors.cyan(` ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++`))
 
 // Function definition
 const colorState = (value = "off") => {
@@ -11,7 +15,7 @@ const colorState = (value = "off") => {
         console.log(colors.rainbow(`9 / 3 = ${calc.divide(9,3)}`))
         console.log(colors.rainbow(`10 * 89 = ${calc.multiply(10,89)}`))
         console.log(colors.rainbow(`40 % 3 = ${calc.modulo(40,3)}`))
-        
+
     } else {
         console.log(`4 + 2 = ${calc.add(4,2)}`)
         console.log(`8 - 12 = ${calc.substract(8,12)}`)
@@ -21,14 +25,54 @@ const colorState = (value = "off") => {
     }
 }
 
+const fetchFollowers = (uname) => {
+    const url = `https://api.github.com/users/ariebrainware/followers`
+    fetch(url)
+        .then(Response => {
+            Response.json()
+        })
+        .then(data => {
+            console.log(colors.blue(`[v] This is ${uname} followers list: `))
+            console.log(data)
+            // data.forEach(user => {
+            //     console.log(user.login)
+            // });
+        })
+}
 // Get user input
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
-input.question('Do you want colorful output?: ', (answer) =>{
-    if(answer == 'yes'){ colorState("on")} 
-    else if(answer == 'no'){ colorState("off")}
-    input.close()
-} )
+input.question(colors.yellow('[?] What do you want to do?, [A] Running math function or [B] See github followers (A/B): '), (answer) => {
+    if (answer === 'A') {
+        console.log(colors.green(`[+] You selected A option, running math function`))
+        input.question(colors.yellow(`[?] Do you want colorful output? [yes/no]: `), (answer) => {
+            if (answer == 'yes') {
+                colorState("on")
+            } else if (answer == 'no') {
+                colorState("off")
+            } else {
+                console.log(colors.red(`[x] Doesnt recognize the input, please choose A or B!`))
+                console.log(colors.red(`[x] PROGRAM TERMINATED!!`))
+                input.close()
+            }
+            // input.close()
+        })
+    } else if (answer === 'B') {
+        console.log(console.log(colors.green(`[+] You selected B option, running fetch followers function`)))
+        input.question(colors.yellow('[?] Input username target: '), uname => {
+            if (uname != "") {fetchFollowers(uname)}
+            else {
+                console.log(colors.red(`[x] Doesnt recognize the input, please choose A or B!`))
+                console.log(colors.red(`[x] PROGRAM TERMINATED!!`))
+                input.close()
+            }
+            input.close()
+        })
+    } else {
+        console.log(colors.red(`[x] Doesnt recognize the input, please choose A or B!`))
+        console.log(colors.red(`[x] PROGRAM TERMINATED!!`))
+        input.close()
+    }
+})
